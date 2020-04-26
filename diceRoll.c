8753b9382de27952bbe73520a6646f8c1b4a3118
Tuomas Lähteenmäki <lahtis@users.noreply.github.com>
2020-04-26 10:56:42 +0300

    Update diceRoll.c
    Intializes random number generator

--- diceRoll.c
+++ diceRoll.c
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 
 // Random 1-6 diceRoll generator with C code.
 
@@ -11,6 +12,10 @@ int main()
 
 	int i;
 	int diceRoll;
+	time_t t;
+
+    	/* Intializes random number generator */
+    	srand((unsigned) time(&t));
 
 	for(i=0; i<6; i++) 
 	{
