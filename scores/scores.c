49ec8e0cbc3470fb3490ef87bd1eb48332985722
Tuomas Lähteenmäki <lahtis@users.noreply.github.com>
2020-05-09 21:40:45 +0300

    update line read
    Read names in line and count characters and return the results program use.

--- scores/scores.c
+++ scores/scores.c
@@ -6,11 +6,13 @@
 
     Counts all lines.
     Read from the beginning of the line how many characters are stored.
+    Read names in line and count characters and return the results program use.
     Use malloc function. (heap-> write memory and clean it.)
-    Print it all.
+    Print it all to memory.
 
     gcc main.c -o main
 */
+int count_char(int a);
 
 
 typedef struct
@@ -59,11 +61,14 @@ int main() {
 
     for (int i = 0; i < personCount; i++)
     {
-        int nameLen;
+        int names;
+        int n1;
 
-        fscanf(file, "%i", &nameLen);  // Read the name lenght the first column of the row.
+        fscanf(file, "%i", &names); // Read the name to file.
 
-        entries[i].name = malloc(sizeof(char) * (nameLen + 1));
+        n1 = count_char(names); // Put name to function, count characters and return the results.
+
+        entries[i].name = malloc(sizeof(char) * (n1 + 1));
         fscanf(file, "%s %i", entries[i].name, &entries[i].score);
 
         printf("%i. %s - %i\n", i + 1, entries[i].name, entries[i].score);
@@ -78,5 +83,17 @@ int main() {
     free(entries);
 
     return 0;
+}
+
+int count_char(int a) { // Function definition
+    int countResults = 0, i;
+
+    for (int i = 0; a != '\0'; i++){
+
+        if (a == ' ' && a != ' ')
+        countResults++;
+
+    }
+    return countResults;
 }
 
