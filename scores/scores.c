adb8271dff1894e094509f8eea97f9aff6d86010
Tuomas Lähteenmäki <lahtis@users.noreply.github.com>
2020-05-09 22:13:41 +0300

    Update scores.c

--- scores/scores.c
+++ scores/scores.c
@@ -1,13 +1,13 @@
 /* 
 Simple C -program that reads score results from a text file.
 
-    Counts all lines.
-    Read from the beginning of the line how many characters are stored.
-    Read names in line and count characters and return the results program use.
-    Use malloc function. (heap-> write memory and clean it.)
-    Print it all to memory.
+Counts all lines.
+Read from the beginning of the line how many characters are stored.
+Read names in line and count characters and return the results program use.
+Use malloc function. (heap-> write memory and clean it.)
+Print it all to memory.
 
-    gcc main.c -o main
+gcc main.c -o main
 
 Copyright (C) (C) Tuomas Lähteenmäki, 2020
 
