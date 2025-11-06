98dfdbde341e05eee1bd1edc810ff9b7680d448e
Tuomas Lähteenmäki <lahtis@users.noreply.github.com>
2025-11-06 12:26:04 +0200

    Refactor version string handling and output

--- version_string-a.c
+++ version_string-a.c
@@ -1,24 +1,15 @@
-/* Tulostuksen yhdistäminen
-​Jos VERSION_STRING on määritelty esimerkiksi muodossa 1.0.5, voit upottaa sen muihin tulostusmerkkijonoihin C-kielen ominaisuuden avulla: vierekkäiset merkkijonoliteraalit yhdistyvät automaattisesti yhdeksi merkkijonoksi.
-*/
-
-#define VERSION_STRING  "1.0.5" // Tässä on jo merkkijono
-#define VER_PREFIX "Ohjelman versio: "
-
-printf(VER_PREFIX XSTR(VERSION_STRING) "\n");
-// Esikääntäjän jälkeen (ja automaattisen yhdistämisen ansiosta):
-// printf("Ohjelman versio: 1.0.5\n"); 
-
-/*
-Jos VERSION_STRING olisi numeerinen, tarvitaan edellä mainitut STR ja XSTR makrot:
-*/
-
+// Erilliset numeeriset määritelmät
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 0
-#define VERSION_PATCH 5
+#define VERSION_MINOR 5
+#define VERSION_PATCH 23
 
+// ... ja mahdollisesti jopa kokonaislukuarvo helppoa vertailua varten:
+#define VERSION (VERSION_MAJOR * 10000 + VERSION_MINOR * 100 + VERSION_PATCH) 
+
+// Yhdistäminen tulostusmerkkijonoksi
 #define STR(x) #x
 #define XSTR(x) STR(x)
+#define VERSION_STRING XSTR(VERSION_MAJOR) "." XSTR(VERSION_MINOR) "." XSTR(VERSION_PATCH)
 
-printf("Versio: %s.%s.%s\n", XSTR(VERSION_MAJOR), XSTR(VERSION_MINOR), XSTR(VERSION_PATCH));
-// Tulostaa: Versio: 1.0.5
+// Tulostus:
+printf("Versio: %s\n", VERSION_STRING); // Tulostaa: Versio: 1.5.23 
